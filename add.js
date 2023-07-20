3d5348ced00e0932b01b5aa16b1037d71247519f
Dipeshxman11 <dipeshramane9378>
2023-07-20 20:06:20 +0530

    first commit

---
+++ add.js
@@ -0,0 +1,6 @@
+let a1=15;
+let b1=12;
+
+let add = (a,b) => a+b;
+
+console.log(add(a1,b1));
---
+++ object.js
@@ -0,0 +1,9 @@
+const student = {
+    age : 20,
+    id : 1,
+    studentdetails(){
+       
+         console.log("Hi My id is " + this.id  + " and age is " + this.age);   
+    }
+}
+student.studentdetails();
